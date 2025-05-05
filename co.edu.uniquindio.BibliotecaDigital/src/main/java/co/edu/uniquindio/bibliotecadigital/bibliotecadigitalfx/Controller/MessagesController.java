d7f1f20aa89665bc81ad8220d9f7390681d233fa
karenrestrepo <142433310+karenrestrepo@users.noreply.github.com>
2025-05-05 11:46:47 -0500

    Interfaz Messages

--- co.edu.uniquindio.BibliotecaDigital/src/main/java/co/edu/uniquindio/bibliotecadigital/bibliotecadigitalfx/Controller/MessagesController.java
+++ co.edu.uniquindio.BibliotecaDigital/src/main/java/co/edu/uniquindio/bibliotecadigital/bibliotecadigitalfx/Controller/MessagesController.java
@@ -1,4 +1,51 @@
 package co.edu.uniquindio.bibliotecadigital.bibliotecadigitalfx.Controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class MessagesController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnSend;
+
+    @FXML
+    private ComboBox<?> cbFriends;
+
+    @FXML
+    private ListView<?> lvMessages;
+
+    @FXML
+    private TextField txtMessages;
+
+    @FXML
+    void onChat(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSend(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnSend != null : "fx:id=\"btnSend\" was not injected: check your FXML file 'Messages.fxml'.";
+        assert cbFriends != null : "fx:id=\"cbFriends\" was not injected: check your FXML file 'Messages.fxml'.";
+        assert lvMessages != null : "fx:id=\"lvMessages\" was not injected: check your FXML file 'Messages.fxml'.";
+        assert txtMessages != null : "fx:id=\"txtMessages\" was not injected: check your FXML file 'Messages.fxml'.";
+
+    }
+
 }
--- co.edu.uniquindio.BibliotecaDigital/src/main/java/co/edu/uniquindio/bibliotecadigital/bibliotecadigitalfx/Controller/SuggestedFriendsController.java
+++ co.edu.uniquindio.BibliotecaDigital/src/main/java/co/edu/uniquindio/bibliotecadigital/bibliotecadigitalfx/Controller/SuggestedFriendsController.java
@@ -1,4 +1,26 @@
 package co.edu.uniquindio.bibliotecadigital.bibliotecadigitalfx.Controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class SuggestedFriendsController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ListView<?> lvSuggestedFriends;
+
+    @FXML
+    void initialize() {
+        assert lvSuggestedFriends != null : "fx:id=\"lvSuggestedFriends\" was not injected: check your FXML file 'SuggestedFriends.fxml'.";
+
+    }
+
 }
+
